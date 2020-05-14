b6f4c4b57feeb91480443c08238abcd53325a15b
Mac <mac@MacdeiMac.local>
2020-05-14 14:25:27 +0800

    提交

--- flutter_animator_tabbar/README.md
+++ flutter_animator_tabbar/README.md
@@ -1,6 +1,49 @@
 # flutter_animator_tabbar
 
-A new Flutter package project.
+仿美团的tabbar效果
+
+## Example
+
+```
+class RootPage extends StatefulWidget {
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+
+  final PageController _controller = PageController(initialPage: 2);
+  int index = 2;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: [
+          HomeScreen("abc"),
+          HomeScreen("bbc"),
+        ],
+      ),
+      bottomNavigationBar: TabAnimationBar(
+        index: index,
+        backgroundColor: Colors.white,
+        children: List.generate(5, (index) => Icon(Icons.home, size: 30,)),
+        selectedChildren: List.generate(5, (index) => Icon(Icons.image, size: 30,color: Colors.cyan,)),
+        onPress: (int selected){
+          _controller.jumpToPage(selected);
+        },
+      ),
+    );
+  }
+}
+```
 
 ## Getting Started
 
--- flutter_animator_tabbar/lib/flutter_animator_tabbar.dart
+++ flutter_animator_tabbar/lib/flutter_animator_tabbar.dart
@@ -2,13 +2,22 @@ library flutter_animator_tabbar;
 
 import 'package:flutter/material.dart';
 
+///
+/// 修改duration 、 curve、 circleOffset 属性的时候需要reload
+///
+///
 class TabAnimationBar extends StatefulWidget {
 
-  final Color backgroundColor;
+  final Color backgroundColor;            // tabbarView的背景颜色
   final List<Widget> children;
   final List<Widget> selectedChildren;
   int index;
   final Function(int selected) onPress;
+  final Color dividerColor;               // 分割线颜色
+  final double circleOffset;              // 圆突出的范围10 - 30
+  final double circleRadiuOffset;         // 圆大小的偏移
+  final Duration animateDuration; 
+  final Curve curve; 
 
   TabAnimationBar({
       Key key, 
@@ -16,7 +25,12 @@ class TabAnimationBar extends StatefulWidget {
       @required this.children, 
       @required this.onPress, 
       @required this.selectedChildren,
-      this.index = 0
+      this.dividerColor = Colors.black12,
+      this.index = 0,
+      this.circleOffset = 10,
+      this.circleRadiuOffset = 0,
+      this.animateDuration = const Duration(milliseconds: 200),
+      this.curve = Curves.bounceOut
     })
      : super(key: key);
 
@@ -32,9 +46,9 @@ class _TabAnimationPageState extends State<TabAnimationBar> with SingleTickerPro
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 200));
-    CurvedAnimation _cure = CurvedAnimation(curve: Curves.bounceOut, parent: _controller);
-    _animation = Tween<double>(begin: 0, end: 10).animate(_cure)
+    _controller = AnimationController(vsync: this, duration: widget.animateDuration);
+    CurvedAnimation _cure = CurvedAnimation(curve: widget.curve, parent: _controller);
+    _animation = Tween<double>(begin: 0, end: widget.circleOffset).animate(_cure)
     ..addListener((){
       setState(() {
         
@@ -65,7 +79,7 @@ class _TabAnimationPageState extends State<TabAnimationBar> with SingleTickerPro
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final margin = MediaQuery.of(context).padding;
-    final height = 40 + margin.bottom;
+    final height = 44 + margin.bottom;
     return Stack(
       children: [
         CustomPaint(
@@ -74,8 +88,10 @@ class _TabAnimationPageState extends State<TabAnimationBar> with SingleTickerPro
           index: widget.index,
           valY: _animation.value,
           color: widget.backgroundColor,
-          childrenCount: widget.children.length
-        )),
+          circleRadiusOffset: widget.circleRadiuOffset,
+          childrenCount: widget.children.length,
+          dividerColor: widget.dividerColor
+        ),),
         Container(
           height: height,
           padding: EdgeInsets.only(bottom: margin.bottom / 3),
@@ -98,8 +114,17 @@ class WaveTabbar extends CustomPainter {
   final double valY;
   final Color color;
   final int childrenCount;
+  final Color dividerColor;
+  final double circleRadiusOffset;
 
-  WaveTabbar({this.index, this.valY, this.color, this.childrenCount});
+  WaveTabbar({
+    this.index, 
+    this.valY, 
+    this.color, 
+    this.childrenCount,
+    this.dividerColor = Colors.black12,
+    this.circleRadiusOffset = 0
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -107,11 +132,13 @@ class WaveTabbar extends CustomPainter {
     ..color = color
     ;
     final circleY = -valY;
+
+    double circleWidth = size.height + circleRadiusOffset;
     // 计算间距
-    double distance = (size.width / childrenCount - size.height) / 2.0;
+    double distance = (size.width / childrenCount - circleWidth) / 2.0;
 
     final paint2 = Paint()
-    ..color = Colors.black12
+    ..color = dividerColor
     ..strokeWidth = 0.5
     ..style = PaintingStyle.stroke
     ;
@@ -119,11 +146,11 @@ class WaveTabbar extends CustomPainter {
 
     // 绘制圆
     Path path = Path()
-    ..addOval(Rect.fromLTWH((size.width) / childrenCount * index + distance, circleY, size.height, size.height));
+    ..addOval(Rect.fromLTWH((size.width) / childrenCount * index + distance, circleY, circleWidth, circleWidth));
     canvas.drawPath(path, paint);
 
     // 绘制圆的边框
-    double circleRadiu = size.height / 2.0 + 0.5;
+    double circleRadiu = circleWidth / 2.0 + 0.5;
     canvas.drawCircle(Offset((size.width) / childrenCount * index + distance + circleRadiu , circleY + circleRadiu), circleRadiu, paint2);
   }
 
